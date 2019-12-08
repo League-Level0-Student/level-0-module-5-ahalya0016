9bf251c58e813ece07395b980fbe52b76eb5961a
League Student <league@iMac-37.attlocal.net>
2019-12-07 17:28:35 -0800

    #4 For loop guantlent

--- src/_06_for_loop_gauntlet/For_loop_gauntlent.java
+++ src/_06_for_loop_gauntlet/For_loop_gauntlent.java
@@ -9,13 +9,40 @@ public static void main(String[] args) {
 	
 //NESTED FOR LOOPS:	
 //NUMBER 1:
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			System.out.println(i);
-			System.out.println(j);
+	/*for (int i = 0; i < 3; i++) {
+	for (int j = 0; j < 3; j++) {
+			System.out.println(i+" " +j);   
 		}
-	}
+	}*/
 	
+	/*for(int i = 1; i <8; i+=3) {
+		String o = "";
+	for (int j = i+1;j<i+3; j++) { 
+o=o +  " " + j;
+		
+	}
+	System.out.println(i + " " + o );
+		}*/
+	
+	/*for(int i = 1; i <100; i+=10) {
+	String o = "";
+for (int j = i+1;j<i+10; j++) { 
+o=o +  " " + j;
+	
+}
+System.out.println(i + " " + o );
+	}*/
+	
+String o = "";
+	for(int i = 1; i<7; i+=1) {
+	for (int j = i+1; j<i+6; j++) {
+		o=o + " " + j;
+}
+
+System.out.println(i + "" + o);
+}
 
 }
+
 }
+
